513b876eaea70864d1b22865c322e4609015adce
posledniyemonazemle <61cmapk@mail.ru>
2025-04-29 15:44:30 +0300

    Update LocalizationTest.cs

--- LocalizationTest.cs
+++ LocalizationTest.cs
@@ -1,6 +1,6 @@
 using UnityEngine;
 using Scripts.Localization;
-
+// Just a test class, don't pay any attention.
 public sealed class LocalizationTest : MonoBehaviour
 {
     public void Initialize()
