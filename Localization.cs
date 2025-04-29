72a0fa22e75fc29db56b007f5508d62c2db29d54
posledniyemonazemle <61cmapk@mail.ru>
2025-04-29 15:38:24 +0300

    Update Localization.cs

--- Localization.cs
+++ Localization.cs
@@ -7,7 +7,7 @@ namespace Scripts.Localization
         private Dictionary<string, string> _language = new Dictionary<string, string>();
 
         public void AddPhrase(string key, string value) => _language.Add(key, value);
-        public void RemovePhrase(string key, string value) => _language.Remove(key);
+        public void RemovePhrase(string key) => _language.Remove(key);
 
         public string GetPhrase(string key) => _language[key];
     }
